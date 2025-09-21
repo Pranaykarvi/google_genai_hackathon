b489805d5f72693b5c576f5df5c21504b64c585b
Pranay Kumar Karvi <pranaykarvi@gmail.com>
2025-09-21 12:37:25 +0530

    Update page.tsx

--- main_website/app/deepfake/page.tsx
+++ main_website/app/deepfake/page.tsx
@@ -57,7 +57,7 @@ export default function DeepfakeDetection() {
     formData.append("files", file) // key must match backend expecting "files"
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/detect/", {
+      const response = await fetch("https://falsify-backend.onrender.com/detect/", {
         method: "POST",
         body: formData,
       })
